e655131d3d065f58dd830b36dc8b02acbd0ce23c
sayan-bose <sayannbose@gmail.com>
2022-08-13 00:54:00 +0530

    ignore eslint during builds

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  
+
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ['res.cloudinary.com'],
   },
 
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
 
     return config;
   },
-}
 
-
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
