e9a1d1f07069fcc0787e2eca8b91fecb5838d72f
sayan-bose <sayannbose@gmail.com>
2022-08-15 15:15:05 +0530

    fixed laoder animation

--- client/src/components/ui/loader/index.tsx
+++ client/src/components/ui/loader/index.tsx
@@ -16,19 +16,15 @@ export const Wrapper = styled.div`
 interface BoxProps {
   index: number;
 }
-
+/* animation: ${bounedelay} 1.4s infinite ease-in-out both; */
 export const Box = styled.div`
-  /* width: 50px;
-  height: 10px;
-  background-color: #25153a; */
-
   display: inline-block;
   margin: 0px 5px;
   animation: ${bounedelay} 1.4s infinite ease-in-out both;
   animation-delay: ${(props: BoxProps) => `${-0.32 + props.index * 0.16}s`};
 `;
 
-interface LoaderProps {
+/* interface LoaderProps {
   theme: 'dark' | 'light';
   size: 'sm' | 'md' | 'lg';
 }
@@ -37,14 +33,9 @@ const SIZE_MAP = {
   sm: '2',
   md: '4',
   lg: '6',
-};
+}; */
 
 export const Loader = (props) => {
-  const { theme = 'dark', size = 'sm' } = props;
-
-  const bgColor = theme === 'dark' ? '#49266a' : '#ff9e48';
-  const sizeDim = SIZE_MAP[size];
-
   return (
     <Wrapper
       {...props}
@@ -54,7 +45,7 @@ export const Loader = (props) => {
         <Box
           key={`Loading-Box-${elem}`}
           index={elem}
-          className={`bg-[${bgColor}] rounded-full w-${sizeDim} h-${sizeDim}`}
+          className={`bg-[#49266a] rounded-full w-2 h-2`}
         />
       ))}
     </Wrapper>
